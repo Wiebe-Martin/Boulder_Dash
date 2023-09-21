cae64d61389602356052cc5cbb8a80da7df010e9
j0fr3y <46280748+j0fr3y@users.noreply.github.com>
2023-09-21 07:43:27 +0200

    Klasse DirtBlock erstellt und Tile verbessert

---
+++ core/src/com/mygdx/game/DirtBlock.java
@@ -0,0 +1,10 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class DirtBlock extends Tile{
+
+    public DirtBlock(int y, int x, Texture texture) {
+        super(y, x, texture);
+    }
+}
--- core/src/com/mygdx/game/Tile.java
+++ core/src/com/mygdx/game/Tile.java
@@ -1,6 +1,24 @@
 package com.mygdx.game;
 
-public abstract class Tile {
+import com.badlogic.gdx.graphics.Texture;
+
+public class Tile {
+    public Tile(int y, int x, Texture texture) {
+        this.y = y;
+        this.x = x;
+        this.texture = texture;
+    }
+
+    public Tile(int y, int x, Tile tileBelow, Tile tileAbove, Tile tileLeft, Tile tileRight, Texture texture) {
+        this.y = y;
+        this.x = x;
+        this.tileBelow = tileBelow;
+        this.tileAbove = tileAbove;
+        this.tileLeft = tileLeft;
+        this.tileRight = tileRight;
+        this.texture = texture;
+    }
+
     public int y;
     public int x;
     public Tile tileBelow;
@@ -8,6 +26,7 @@ public abstract class Tile {
     public Tile tileLeft;
     public Tile tileRight;
 
+    public Texture texture;
 
 
 }
