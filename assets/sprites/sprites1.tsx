2f6f38e4a1fcb424a7fcb5d00f750acd6b0b3101
Martin Wiebe <m.wiebe311204@gmail.com>
2023-09-25 20:23:06 +0200

    Test

---
+++ assets/sprites/sprites1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="sprites1" tilewidth="32" tileheight="32" tilecount="290" columns="10">
+ <image source="sprites/sprites.png" width="320" height="952"/>
+</tileset>
--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -11,56 +11,55 @@ import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
 public class MyGdxGame extends ApplicationAdapter {
-	private OrthographicCamera camera;
-	private Viewport viewport;
-	private TiledMap tiledMap;
-	private OrthogonalTiledMapRenderer mapRenderer;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer mapRenderer;
 
-	@Override
-	public void create() {
-		float viewportWidth = 480; // Change to your desired viewport width
-		float viewportHeight = 320; // Change to your desired viewport height
+    @Override
+    public void create() {
+        float viewportWidth = 800; // Change to your desired viewport width
+        float viewportHeight = 600; // Change to your desired viewport height
 
-		// Initialize the camera and viewport
-		camera = new OrthographicCamera();
-		viewport = new FitViewport(viewportWidth, viewportHeight, camera);
-		viewport.apply(true);
+        // Initialize the camera and viewport
+        camera = new OrthographicCamera();
+        viewport = new FitViewport(viewportWidth, viewportHeight, camera);
+        viewport.apply(true);
 
-		// Load your Tiled map
-		tiledMap = new TmxMapLoader().load("map.tmx");
+        // Load your Tiled map
+        tiledMap = new TmxMapLoader().load("maps/map0.tmx");
+        mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
 
-		mapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+        // Set the camera position to center it on the map
+        camera.position.set(viewportWidth / 2, viewportHeight / 2, 0);
+    }
 
-		// Set the camera position to center it on the map
-		camera.position.set(viewportWidth / 2, viewportHeight / 2, 0);
-	}
+    @Override
+    public void render() {
+        // Clear the screen
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-	@Override
-	public void render() {
-		// Clear the screen
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        // Update the camera and mapRenderer
+        camera.update();
+        mapRenderer.setView(camera);
 
-		// Update the camera and mapRenderer
-		camera.update();
-		mapRenderer.setView(camera);
+        // Render the Tiled map
+        mapRenderer.render();
 
-		// Render the Tiled map
-		mapRenderer.render();
+        // Your game rendering logic can go here
+        // For example, you can draw other game objects on top of the Tiled map
+    }
 
-		// Your game rendering logic can go here
-		// For example, you can draw other game objects on top of the Tiled map
-	}
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height);
+    }
 
-	@Override
-	public void resize(int width, int height) {
-		viewport.update(width, height);
-	}
-
-	@Override
-	public void dispose() {
-		// Dispose of resources when the game is closed
-		tiledMap.dispose();
-		mapRenderer.dispose();
-	}
+    @Override
+    public void dispose() {
+        // Dispose of resources when the game is closed
+        tiledMap.dispose();
+        mapRenderer.dispose();
+    }
 }
