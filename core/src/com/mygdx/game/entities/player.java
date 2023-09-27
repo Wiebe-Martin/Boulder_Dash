ac208497eaf1625a0f7b533e459f4e68fe0aa3b0
Martin Wiebe <m.wiebe311204@gmail.com>
2023-09-27 10:42:35 +0200

    Player kann sich bewegen

--- core/src/com/mygdx/game/entities/player.java
+++ core/src/com/mygdx/game/entities/player.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 
 public class Player extends Entity{
-    private float moveSpeed = 2.0f;
+    //private float moveSpeed = 2.0f;
 
     public Player(TiledMap map, int startX, int startY) {
         super(map, startX, startY);
@@ -16,25 +16,35 @@ public class Player extends Entity{
     }
 
     public void update(float deltaTime) {
-        // Implement player-specific update logic here
         handleInput();
     }
 
-    public void handleInput() {
-        float speed = moveSpeed;
+    public void move(int newTileX, int newTileY) {
+        // Check if the new tile coordinates are within the map boundaries
+        if (newTileX >= 0 && newTileX < collisionLayer.getWidth() &&
+            newTileY >= 0 && newTileY < collisionLayer.getHeight()) {
     
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            x -= speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            x += speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
-            y += speed;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
-            y -= speed;
+            this.tileX = newTileX;
+            this.tileY = newTileY;
+        
+            // Update the player's actual position based on the new tile position
+            this.x = tileX * collisionLayer.getTileWidth();
+            this.y = tileY * collisionLayer.getTileHeight();
         }
     }
-    
+
+    public void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            this.move(tileX - 1, tileY); // Move left
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            this.move(tileX + 1, tileY); // Move right
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+            this.move(tileX, tileY + 1); // Move up
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+            this.move(tileX, tileY - 1); // Move down
+        }
+    }  
 }
