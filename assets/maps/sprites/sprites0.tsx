b54e65b235441a1d6836f25dc5dfec8a145232c9
Martin Wiebe <m.wiebe311204@gmail.com>
2023-09-28 19:13:35 +0200

    map1 Objekte hinzugefügt

--- assets/maps/sprites/sprites0.tsx
+++ assets/maps/sprites/sprites0.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="sprites0" tilewidth="32" tileheight="32" tilecount="290" columns="10">
- <image source="sprites.png" width="320" height="952"/>
+ <image source="sprites/sprites.png" width="320" height="952"/>
  <tile id="0" type="Player"/>
  <tile id="70" type="Stone"/>
  <tile id="71" type="Dirt"/>
